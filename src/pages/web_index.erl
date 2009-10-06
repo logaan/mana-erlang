0f61816ac55c5e2f57830455df56a59559273980
Colin Campbell-McPherson <colin@logaan.net>
2009-10-07 01:13:26 +0800

    chucked in some data binding

--- src/pages/web_index.erl
+++ src/pages/web_index.erl
@@ -1,14 +1,26 @@
 -module (web_index).
 -include_lib ("nitrogen/include/wf.inc").
+-include("../records.hrl").
 -compile(export_all).
 
 main() -> 
-	#template { file="./wwwroot/template.html"}.
+  #template { file="./wwwroot/template.html"}.
 
 title() ->
-        oracle:greet().
+  oracle:greet().
 
 body() ->
-	#label{text="web_index body."}.
-	
+  Data = oracle:all_cards(),
+  Map = #card{ name=nameLabel@text, cost=costLabel@text },
+  [
+    #label{text="web_index body."},
+    #bind{ id=oracleBinding, data=Data, map=Map, body=[
+      #panel{ body=[
+        #hr{},
+        #label{ id=nameLabel },
+        #label{ id=costLabel }
+      ]}
+    ]}
+  ].
+
 event(_) -> ok.
